9616cdda41df0e2cc470ef84f03ab4c7842abc51
donguk585 <137376258+donguk585@users.noreply.github.com>
2026-04-13 09:33:04 +0900

    최초 프로젝트

---
+++ miwool/src/main/java/com/taekwondo/miwool/MiwoolApplication.java
@@ -0,0 +1,13 @@
+package com.taekwondo.miwool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiwoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiwoolApplication.class, args);
+	}
+
+}
---
+++ miwool/src/test/java/com/taekwondo/miwool/MiwoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taekwondo.miwool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiwoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
